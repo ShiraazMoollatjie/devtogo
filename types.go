f126c78e8c747d3ffd0bfbaf813e8f72b84ab8a4
Shiraaz Moollatjie <shiraaz@luno.com>
2020-08-04 12:37:22 +0200

    types: Fix types

--- types.go
+++ types.go
@@ -2,15 +2,14 @@ package devtogo
 
 import "time"
 
-// Empty time is a type where the time can be an empty string.
+// emptyTime is a type where the time can be an empty string.
 type emptyTime struct {
 	*time.Time
 }
 
 // UnmarshalJSON is a copy of the time.Time UnmarshalJSON function
-// with an extra check for an empty string
+// with an extra check for an empty string.
 func (m *emptyTime) UnmarshalJSON(data []byte) error {
-
 	if string(data) == "null" || string(data) == `""` {
 		return nil
 	}
